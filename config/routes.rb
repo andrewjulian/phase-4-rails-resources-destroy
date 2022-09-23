7c1738d7523ef66bcfb68e9d0fbfde54bba92fa5
+ <+>
2022-09-23 12:39:49 -0600

    commit tree 00f19e760b569de3cbff684b69a75fa7517f4920

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+
+  #resources :birds, only: [:index, :show, :create, :update, :destroy]
+  
+  #we dont need the restricted resources as we are using all route types
+  resources :birds
+  
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
